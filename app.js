ef1c5a9f6506c873b2320d7bfd41e5f633e0470d
Juan Carlos Ruiz <jcruizlop@gmail.com>
2023-12-13 16:23:17 -0500

    update desde vscode

--- app.js
+++ app.js
@@ -1,7 +1,4 @@
 /*Esto es un comentario mascota*/
 /*Esto es un comentario mascota*/
 /*Esto es un comentario mascota*/
-/*Esto es un comentario mascota*/
-/*Esto es un comentario mascota*/
-/*Esto es un comentario mascota*/
 
